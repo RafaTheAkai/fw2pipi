ebc3e793688932e308299c866744ff9d4838fdcd
RafaTheAkai <149427593+RafaTheAkai@users.noreply.github.com>
2025-12-09 10:26:31 -0400

    Remove db connection setup from dbconnet.js

--- db/dbconnet.js
+++ db/dbconnet.js
@@ -1,16 +1,2 @@
+badwa
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const app = require('./server');
-const uri = process.env.dbconn;
-
-module.exports = (app) => {
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-  app.dbClient = client
-
-}
